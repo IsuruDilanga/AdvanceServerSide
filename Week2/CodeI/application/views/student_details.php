c739e7dd8894e120358c3db64e6c2c351decc7bd
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2024-02-06 23:27:08 +0530

    Tutorial 2 Exercise 2

---
+++ Week2/CodeI/application/views/student_details.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Student Details</title>
+</head>
+<body>
+	<div>
+		<h1>Student Details</h1>
+		<p>Student Name: Isuru Dissanayake</p>
+		<p>University: IIT</p>
+		<p>Student ID: 20211132</p>
+	</div>
+</body>
+</html>
