36a5b08dcd6897d2885274cd06d966a189aacf6e
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2024-03-24 07:59:11 +0530

    added Ex 2 week7

--- Week4/Week4E/application/views/pages/genreMoviesList.php
+++
@@ -1,8 +0,0 @@
-<h1>Genre Movie</h1>
-<div>
-	<?php foreach ($genre_details as $detail) : ?>
-		<p><a href="<?php echo ('http://localhost/Week4E/index.php/pages/'.$detail['genre']); ?>"><?php echo $detail['genre']; ?></a></p>
-	<?php endforeach; ?>
-</div>
-
-<a href="http://localhost/Week4E/index.php/films/getAllFilmDetails">Back All Films Rating</a>
