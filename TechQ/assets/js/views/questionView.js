f7924e0a2f7740a6409dfd12d9d5ad57be27103e
Isuru Dissanayake <98075833+IsuruDilanga@users.noreply.github.com>
2024-04-20 22:04:01 +0530

    home search

--- TechQ/assets/js/views/questionView.js
+++
@@ -1,9 +0,0 @@
-var app = app || {};
-
-app.views.QuestionView = Backbone.View.extend({
-	el: '#question',
-	render:function (){
-		template = _.template($('#question_template').html());
-		this.$el.append(template(this.model.attributes));
-	}
-});
